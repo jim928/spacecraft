3c69823aa9b20dc6ee46e9990305a724a841e300
zhanqi <714217552@qq.com>
2013-09-16 12:15:25 +0800

    add ship file for test

---
+++ spacecraft/ship.h
@@ -0,0 +1,13 @@
+//
+//  ship.h
+//  spacecraft
+//
+//  Created by 湛奇 on 13-9-16.
+//  Copyright (c) 2013年 湛奇. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface ship : SKSpriteNode
+
+@end
---
+++ spacecraft/ship.m
@@ -0,0 +1,13 @@
+//
+//  ship.m
+//  spacecraft
+//
+//  Created by 湛奇 on 13-9-16.
+//  Copyright (c) 2013年 湛奇. All rights reserved.
+//
+
+#import "ship.h"
+
+@implementation ship
+
+@end
